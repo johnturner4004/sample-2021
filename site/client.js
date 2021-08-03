1ec5b95240a07fe45b286041537757528782b1e6
johnturner4004 <johnturner4004@gmail.com>
2021-08-02 22:20:53 -0400

    move menu button on small screen to not cover banner

--- site/client.js
+++ site/client.js
@@ -1,13 +1,26 @@
 $(document).ready(onReady);
+let open = '<i class="fas fa-bars"></i>';
 
 function onReady() {
   console.log('JS');
   $('.menu').toggle(menuState);
   $('.toggle-menu').on('click', menuOpen);
+  $('.toggle-icon').append(open);
 }
 
 let menuState = false;
+let close = '<i class="fas fa-times"></i>';
 
 function menuOpen() {
   $('.menu').animate({width: 'toggle'}, 500, 'swing');
+  menuState = !menuState;
+  if (menuState) {
+    $('.toggle-icon').fadeOut(100, function(){
+      $('.toggle-icon').empty().append(close).fadeIn();
+    });
+  } else {
+    $('.toggle-icon').fadeOut(100, function(){
+      $('.toggle-icon').empty().append(open).fadeIn();
+    });
+  }
 }

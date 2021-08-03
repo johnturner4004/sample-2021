5557f41f041013999a5e7d06881b4a738a683e00
johnturner4004 <johnturner4004@gmail.com>
2021-08-02 20:36:38 -0400

    make menu and headers

--- site/client.js
+++ site/client.js
@@ -2,4 +2,12 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('JS');
+  $('.menu').toggle(menuState);
+  $('.toggle-menu').on('click', menuOpen);
+}
+
+let menuState = false;
+
+function menuOpen() {
+  $('.menu').animate({width: 'toggle'}, 500, 'swing');
 }
